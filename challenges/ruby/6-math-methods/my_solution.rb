b0e34fbe503b1cd491c6ed034f9eab90553fba52
david <davidndy@gmail.com>
2017-10-17 20:11:29 -0500

    Soluciones de los challenges 6,7,8 y 9

--- challenges/ruby/6-math-methods/my_solution.rb
+++ challenges/ruby/6-math-methods/my_solution.rb
@@ -1,3 +1,18 @@
 # Math Methods
 
 # Tu solucion abajo:
+def add(num1, num2)
+	output = num1+num2;
+end
+
+def subtract(num1, num2)
+	output = num1-num2;
+end
+
+def multiply(num1, num2)
+	output = num1*num2;
+end
+
+def divide(num1, num2)
+	output = num1/num2.to_f;
+end
