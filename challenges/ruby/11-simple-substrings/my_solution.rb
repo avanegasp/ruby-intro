e4f86338e5e7820a7c1a09d75dfd82a2302f2d65
david <davidndy@gmail.com>
2017-10-17 21:03:36 -0500

    Soluciones de los challenges 10 y 11

--- challenges/ruby/11-simple-substrings/my_solution.rb
+++ challenges/ruby/11-simple-substrings/my_solution.rb
@@ -1,4 +1,24 @@
 # Simple Substrings
-
+## Resumen
 # Tu solucion abajo:
+def welcome (address)
+	int = 0
+	pruebaA = 0
+	pruebaC = 0
+	address.each_char do |item|
+		int +=1
+		if item == "C"
+			pruebaC = int;
+		end
+		if item == "A"
+			pruebaA = int;
+		end
+	end
+	if pruebaA-pruebaC == 1
+		message = "Welcome to California"
+	else
+		message ="You should move to California"
+	end	
+end
 
+puts welcome("unCA023")
