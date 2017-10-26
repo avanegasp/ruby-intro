ef45a5cb99b86b4eacd09ad49da453b37632ed4c
avanegasp <arco527@gmail.com>
2017-10-25 20:31:50 -0500

    Almost done tree orange

--- challenges/OOP/orange-tree/source/orange_tree.rb
+++ challenges/OOP/orange-tree/source/orange_tree.rb
@@ -3,27 +3,65 @@ class NoOrangesError < StandardError
 end
 
 class OrangeTree
-  # Envejeze el arbol
-  def age!
-  end
+ # Envejeze el arbol
+ attr_reader :age, :height
+ def initialize
+   @age=0
+   @cosecha=false
+   @oranges=[]
+   @height=0
+   @dead=false
+ end
+ def age!
+   if(@age > 25)
+     @dead=true
+   else
+     @age += 1
+     @height += 0.16
+   end
+ end
 
-  # Devuelve true si hay naranjas en el arbol, false si no
-  def any_oranges?
-  end
+ # Devuelve true si hay naranjas en el arbol, false si no
+ def any_oranges?
+   self.cosecha!
+   if (@age > 3 && @cosecha)
+      true
+   else
+     @cosecha=false
+     false
+   end
+ end
 
-  # Devuelve una naranja si hay una
-  # Dispara un NoOrangesError de lo contrario
-  def pick_an_orange!
-    raise NoOrangesError, "This tree has no oranges" unless self.any_oranges?
+ def cosecha!
+   if (rand(4)== 2 && !@cosecha)
+      (rand(200)+1).times do
+        @oranges<<Orange.new(rand(10)+1)
+      end
+      @cosecha = true
+   else
+      @cosecha
+    end
+ end
 
-    # Logica para sacar naranjas va aca
-  end
-end
+ def dead?
+   @dead
+ end
+ # Devuelve una naranja si hay una
+ # Dispara un NoOrangesError de lo contrario
+ def pick_an_orange!
+   raise NoOrangesError,  "This tree has no oranges" unless self.any_oranges?
+   @cosecha = false if  @oranges.size ==1
+   @oranges.pop
+   # Logica para sacar naranjas va aca
+ end #end pick_an_orange!
+end ## end class OrangeTree
 
 class Orange
-  # Crea una naranja con determinado +diameter+
-  def initialize(diameter)
-  end
+ # Crea una naranja con determinado +diameter+
+attr_reader :diameter
+ def initialize(diameter)
+   @diameter=diameter
+ end
 end
 
 
@@ -36,25 +74,26 @@ tree.age! until tree.any_oranges?
 puts "El arbol tiene #{tree.age} años y mide #{tree.height} metros"
 
 until tree.dead?
-  basket = []
+ basket = []
 
-  # Pone las naranjas en la canasta
-  while tree.any_oranges?
-    basket << tree.pick_an_orange!
-  end
+ # Pone las naranjas en la canasta
+
+ while tree.any_oranges?
+   basket << tree.pick_an_orange!
+ end
 
 
-  avg_diameter = basket.map { |orange| orange.diameter}
-  avg_diameter = avg_diameter.reduce(:+) / basket.length
-  puts "Reposte Año #{tree.age}"
-  puts "Altura arbol: #{tree.height} metros"
-  puts "Cosecha: #{basket.size} naranjas con un diametro promedio de #{avg_diameter} cm"
-  puts ""
+ avg_diameter = basket.map { |orange| orange.diameter}
 
-  # Envejeze el arbol en 1 año
-  #
-  tree.age!
+ avg_diameter = avg_diameter.reduce(:+) / basket.length
+ puts "Reposte Año #{tree.age}"
+ puts "Altura arbol: #{tree.height} metros"
+ puts "Cosecha: #{basket.size} naranjas con un diametro promedio de #{avg_diameter} cm"
+ puts ""
+
+ binding
+ # Envejeze el arbol en 1 año
+ #
+ tree.age!
 end
-
 puts "Miercoles, se murio el arbol!"
-
